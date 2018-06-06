7a06c78317bf0f0cd1e03b5a22c8474f4dea2854
LarisaGl <89888985265@mail.ru>
2018-06-06 17:32:41 +0300

    +

---
+++ books.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BOOKS</title>
+	<meta charset="UTF-8">
+	<style>
+   		table, tr, td { 
+    	border: 1px solid black;
+    	text-align: center;
+   		}
+ 	</style>
+</head>
+<body>
+
+<?php
+
+header ("Content-Type: text/html; charset=utf-8");
+
+include "config.php";
+
+$sql='select * from books';
+
+$res=mysqli_query ($connect, $sql);
+
+echo "<table>";
+echo "<tr>";
+	echo "<td>"."name"."</td>";
+	echo "<td>"."author"."</td>";
+	echo "<td>"."year"."</td>";
+	echo "<td>"."isbn"."</td>";
+	echo "<td>"."genre"."</td>";
+echo "</tr>";
+
+while ($data=mysqli_fetch_assoc ($res)) {
+	echo "<tr>";
+	echo "<td>".$data['name']."</td>";
+	echo "<td>".$data['author']."</td>";
+	echo "<td>".$data['year']."</td>";
+	echo "<td>".$data['isbn']."</td>";
+	echo "<td>".$data['genre']."</td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+?>
+
+
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$connect=mysqli_connect("localhost", "lgolovina", "neto1757", "global");
+
+?>
